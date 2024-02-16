48dcdaba2434bcd53a4220b0b76304edcc897769
Alex-LikesPizza <alexandru13321@gmail.com>
2024-02-16 18:26:55 +0200

    bacterii rep19-11-2

--- rep-9-2/lacata -u/lacata.cpp
+++ rep-9-2/lacata -u/lacata.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <cmath>
 #include <fstream>
 using namespace std;
 
 
 int minMoves = INT_MAX;
+int changeCode(int code, int poz, int by){
+  int temp = code;
+  temp /= pow(10, poz - 1);
+  temp %= 10;
+  code -= temp * int(pow(10, poz - 1));
+  temp = (temp + by) >= 0? (temp + by) % 10: (10 + temp + by) % 10;
+  code += temp * int(pow(10, poz - 1));
+  return code;
+}
 void findCode(){
   
 }
 
 int main(){
 
-    ifstream fin("file.in");
-    ofstream fout("file.out");
+    ifstream fin("lacata.in");
+    ofstream fout("lacata.out");
 
     short code; fin >> code;
     short nBlockCodes; fin >> nBlockCodes;
@@ -19,7 +29,6 @@ int main(){
     for(int i = 0; i < nBlockCodes; i++){
       fin >> blocks[i];
     }
-
 
 
     fin.close();
---
+++ rep19-11-2/Bacterii/bacterii.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int bacterii(bool isBig, int lifespan, int timeRemaining, int originalLifespan){
+  if(timeRemaining == 0) return 0;
+  if(lifespan == 0) return -1;
+  int sum = 0;
+  if(!isBig){
+    sum = bacterii(true, lifespan - 1, timeRemaining - 1, originalLifespan); 
+  }
+  else {
+    sum++;
+    sum += bacterii(false, originalLifespan, timeRemaining - 1, originalLifespan);
+    sum += bacterii(true, lifespan - 1, timeRemaining - 1, originalLifespan);
+  }
+  return sum;
+}
+
+int main(){
+  ifstream fin("bacterii.in");
+  ofstream fout("bacterii.out");
+
+  int n, lifespan, time;
+  fin >> n >> lifespan >> time;
+  int alive = n * bacterii(false, lifespan, time, lifespan);
+  cout << alive;
+
+
+  fin.close();
+  fout.close();
+}
