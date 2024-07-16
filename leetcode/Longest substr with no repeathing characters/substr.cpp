3cb183ac7c62e4be673306337128ed79cd6c436e
Alex-LikesPizza <alexandru13321@gmail.com>
2024-07-16 21:59:40 +0300

    length of longest substring

---
+++ leetcode/Longest substr with no repeathing characters/substr.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+class Solution {
+  private:
+    
+  public:
+    int lengthOfLongestSubstring(std::string s){
+      int maxLength = 0;
+      int length = 0;
+      std::unordered_set<char> letters;
+      
+      auto from = s.begin();
+      auto to = s.begin();
+      letters.insert(s.at(0));
+
+      while (to != s.end()) {
+        if (letters.find(*to) == letters.end()) {
+          letters.insert(*to);
+          to++;
+          int length = to - from;
+          if (length > maxLength) maxLength = length;
+        } else {
+          letters.erase(*from);
+          from++;
+        }
+      }
+
+
+      return maxLength;
+    }
+} solution;
+
+int main(){
+  std::cout << solution.lengthOfLongestSubstring("qwertyuiopqasdfghjklqzcvbnm");
+}
+
+
+/*
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+*/
